170b36edd2a18d8728e0f0b23693a4a7fae1c356
Hayata Suenaga <hayata@expensify.com>
2023-10-11 15:19:27 +0900

    feat: implement basic navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+}
 
 function App() {
+  useEffect(() => {
+    function handleURLChange() {
+      window.parent.postMessage(window.location.href, "http://localhost:8082");
+    }
+
+    window.addEventListener("hashchange", handleURLChange, false);
+
+    const originalPushState = window.history.pushState;
+    window.history.pushState = function () {
+      originalPushState.apply(window.history, arguments);
+      handleURLChange();
+    };
+
+    const originalReplaceState = window.history.replaceState;
+    window.history.replaceState = function () {
+      originalReplaceState.apply(window.history, arguments);
+      handleURLChange();
+    };
+
+    return () => {
+      window.removeEventListener("hashchange", handleURLChange);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/inbox">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
