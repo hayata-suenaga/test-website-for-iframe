e9bec74f60993b06705f68b440c5fa34e8043253
Hayata Suenaga <hayata@expensify.com>
2023-10-11 16:24:46 +0900

    feat: add more paths

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useSearchParams,
+} from "react-router-dom";
 
 function Home() {
   return (
     <div>
-      <h2>Home Page</h2>
-      <Link to="/">Go back to Home</Link>
+      <h2>Home</h2>
     </div>
   );
 }
 
-function About() {
+function Expenses() {
+  const [searchParams] = useSearchParams();
+  const param = JSON.parse(searchParams.get("param")) ?? {};
+  const { viewMode } = param;
+
   return (
     <div>
-      <h2>About Page</h2>
-      <Link to="/">Go back to Home</Link>
+      <h2>{viewMode === "charts" ? "Insights" : "Expenses"}</h2>
+    </div>
+  );
+}
+
+function Reports() {
+  return (
+    <div>
+      <h2>Reports</h2>
+    </div>
+  );
+}
+
+function Settings() {
+  const [searchParams] = useSearchParams();
+  const param = JSON.parse(searchParams.get("param")) ?? {};
+  const { section } = param;
+
+  return (
+    <div>
+      <h2>
+        {section === "individual"
+          ? "Individual Workspaces"
+          : "Group Workspaces"}
+      </h2>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h2>404 Page</h2>
     </div>
   );
 }
 
 function App() {
   useEffect(() => {
+    const isWebsiteEmbedded = window !== window.top;
+
+    if (!isWebsiteEmbedded) {
+      return;
+    }
+
     function handleURLChange() {
       window.parent.postMessage(window.location.href, "http://localhost:8082");
     }
@@ -53,14 +99,38 @@ function App() {
               <Link to="/inbox">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/expenses">Expenses</Link>
             </li>
+            <li>
+              <Link to="/reports">Reports</Link>
+            </li>
+            <li>
+              <Link to='/expenses?param={"viewMode":"charts"}'>Insights</Link>
+            </li>
+            <li>
+              <Link to="/admin_policies">Workspaces</Link>
+            </li>
+            <ul>
+              <li>
+                <Link to='/admin_policies?param={"section":"individual"}'>
+                  Individual Workspaces
+                </Link>
+              </li>
+              <li>
+                <Link to='/admin_policies?param={"section":"group"}'>
+                  Group Workspaces
+                </Link>
+              </li>
+            </ul>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/inbox" element={<Home />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/admin_policies" element={<Settings />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
